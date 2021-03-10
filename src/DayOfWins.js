eb0564fa101232c3d3c9b374f6a902aa80937ad1
BeatriceDuncan <bealmduncan@gmail.com>
2021-03-10 18:46:44 -0500

    added delete win function

--- src/DayOfWins.js
+++ src/DayOfWins.js
@@ -1,3 +1,5 @@
+import firebase from "./firebase";
+
 const DayOfWins = (props) => {
   const {
     dayId,
@@ -5,8 +7,15 @@ const DayOfWins = (props) => {
     win2,
     win3
   } = props.inputs
+
+  const handleClick = (uniqueKey) => {
+    const dbref = firebase.database().ref();
+    dbref.child(uniqueKey).remove();
+  }
+  
   return (
     <li className='eachDay'>
+      <span aria-label="delete these wins" className="deleteWin" onClick={() => {handleClick(props.inputs.uniqueKey)}}>X</span>
       <h3>{dayId}</h3>
       <ol>
         <li><div className="winIcon"></div>{win1}</li>
